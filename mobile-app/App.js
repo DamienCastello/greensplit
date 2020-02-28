e1cd95bcd81efc839256d22ed4a749783b7d881b
DamienCastello <d.castello.13200@gmail.com>
2020-02-28 20:01:57 +0100

    add sandbox component

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { StyleSheet, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from './components/Header';
 import Content from './components/Content';
+//import Sandbox from './components/Sandbox';
 
 export default function App() {
   return (
+    //<Sandbox/>
     <TouchableWithoutFeedback onPress={() => {
       Keyboard.dismiss();
       console.log('dismissed keyboard');
