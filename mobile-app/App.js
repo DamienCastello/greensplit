8e1cb78269b00188d18e291e069c9a5b8816f7d4
DamienCastello <d.castello.13200@gmail.com>
2020-02-28 17:40:09 +0100

    add header

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Header from './components/Header';
+import Content from './components/Content';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <Content />
     </View>
   );
 }
@@ -13,7 +16,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
